9375aeb13a327e5622f91028f7f00a6918d10492
nirch <nirh2@yahoo.com>
2013-09-22 13:00:41 +0300

    RemakeProject implemented and tested (create video is not implemented). The UI can now store the takes in the segment remake object

--- HomagePrototype/Model/HMGRemakeProject.m
+++ HomagePrototype/Model/HMGRemakeProject.m
@@ -7,7 +7,61 @@
 //
 
 #import "HMGRemakeProject.h"
+#import "HMGSegment.h"
+#import "HMGVideoSegment.h"
+#import "HMGImageSegment.h"
+#import "HMGTextSegment.h"
+#import "HMGSegmentRemake.h"
+#import "HMGVideoSegmentRemake.h"
+#import "HMGImageSegmentRemake.h"
+#import "HMGTextSegmentRemake.h"
 
 @implementation HMGRemakeProject
+
+- (id)initWithTemplate:(HMGTemplate *) templateObj
+{
+    self = [super init];
+    
+    if (self)
+    {
+        self.templateObj = templateObj;
+        NSMutableArray *segmentRemakes = [[NSMutableArray alloc] init];
+        
+        // Looping over the segments of the template in order to create the parallel segment remake objects
+        for (HMGSegment *segment in self.templateObj.segments)
+        {
+            HMGSegmentRemake *segmentRemake = [self createSegmentRemakeFrom:segment];
+            [segmentRemakes addObject:segmentRemake];
+        }
+        
+        // Converting the NSMutable array into an NSArray
+        self.segmentRemakes = [NSArray arrayWithArray:segmentRemakes];
+    }
+
+    return self;
+}
+
+// Creating a SegmentRemake object from a given Segment object
+- (HMGSegmentRemake *)createSegmentRemakeFrom:(HMGSegment *)segment
+{
+    HMGSegmentRemake *segmentRemake;
+    
+    if ([segment isKindOfClass:[HMGVideoSegment class]])
+    {
+        segmentRemake = [[HMGVideoSegmentRemake alloc] init];
+    }
+    else if ([segment isKindOfClass:[HMGImageSegment class]])
+    {
+        segmentRemake = [[HMGImageSegmentRemake alloc] init];
+    }
+    else if ([segment isKindOfClass:[HMGTextSegment class]])
+    {
+        segmentRemake = [[HMGTextSegmentRemake alloc] init];
+    }
+    
+    return segmentRemake;
+}
+
+
 
 @end
---
+++ HomagePrototypeLogicTests/RemakeProjectTests.h
@@ -0,0 +1,13 @@
+//
+//  RemakeProjectTests.h
+//  HomagePrototype
+//
+//  Created by Tomer Harry on 9/22/13.
+//  Copyright (c) 2013 Homage. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface RemakeProjectTests : SenTestCase
+
+@end
