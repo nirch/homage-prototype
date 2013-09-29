76841ea4bc3835075b87a4a475e4897339565b0a
nirch <nirh2@yahoo.com>
2013-09-29 21:29:09 +0300

    Resources for testing AVUtils has been added

--- HomagePrototypeTests/AVUtilsTests.m
+++ HomagePrototypeTests/AVUtilsTests.m
@@ -11,14 +11,29 @@
 
 @interface AVUtilsTests : SenTestCase
 
+@property (strong, nonatomic) NSMutableArray *resourcesToDelete;
+
+@property (strong, nonatomic) NSURL *video1;
+@property (strong, nonatomic) NSURL *video2;
+
 @end
+
+static NSString * const video1Name = @"Red.mov";
+static NSString * const video2Name = @"Tikim_Text.mp4";
 
 @implementation AVUtilsTests
 
 - (void)setUp
 {
     [super setUp];
-    // Put setup code here; it will be run once, before the first test case.
+
+    NSString *video1Path = [[NSBundle bundleForClass:[self class]] pathForResource:video1Name ofType:nil];
+    self.video1 = [NSURL fileURLWithPath:video1Path];
+    
+    NSString *video2Path = [[NSBundle bundleForClass:[self class]] pathForResource:video2Name ofType:nil];
+    self.video2 = [NSURL fileURLWithPath:video2Path];
+    
+    self.resourcesToDelete = [[NSMutableArray alloc] init];
 }
 
 - (void)tearDown
@@ -27,13 +42,29 @@
     [super tearDown];
 }
 
-- (void)testExample
+// Testing that the setUp method successfully initialized the properties
+- (void)testSetUp
 {
-    [HMGAVUtils mergeVideos:nil withSoundtrack:nil completion:^(AVAssetExportSession *exporter) {
+    NSLog(@"Video 1 URL: %@", self.video1.description);
+    
+    // Checking that the URL of the video contains the name of the video
+    STAssertTrue([self.video1.description rangeOfString:video1Name].location != NSNotFound, @"Video name:<%@> was not found in URL:<%@>", video1Name, self.video1.description);
+    
+    NSLog(@"Video 2 URL: %@", self.video2.description);
+    
+    // Checking that the URL of the video contains the name of the video
+    STAssertTrue([self.video2.description rangeOfString:video2Name].location != NSNotFound, @"Video name:<%@> was not found in URL:<%@>", video2Name, self.video2.description);
+    
+}
+
+- (void)testMergeVideosNoSoundtrack
+{
+    NSArray *videosToMerge = @[self.video1, self.video2];
+    
+    [HMGAVUtils mergeVideos:videosToMerge withSoundtrack:nil completion:^(AVAssetExportSession *exporter) {
         NSLog(@"Exporter Status :%d", exporter.status);
         NSLog(@"Exporter URL: %@", exporter.outputURL.description);
     }];
-   // STFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
 }
 
 @end
